0c61a103fbe998e05f40e819eb3d741aaa9de5b8
Mateusz Jaje <mateuszjaje@gmail.com>
2015-05-02 00:15:25 +0200

    adding start methods

--- src/main/scala/io/github/morgaroth/gdrivesync/parallel/Application.scala
+++ src/main/scala/io/github/morgaroth/gdrivesync/parallel/Application.scala
@@ -2,12 +2,18 @@ package io.github.morgaroth.gdrivesync.parallel
 
 import java.io.File
 
+import akka.actor.ActorSystem
+import com.typesafe.config.ConfigFactory
+import io.github.morgaroth.gdrivesync.parallel.actors.Master
+
 object Application {
   def main(args: Array[String]) {
     val path = if (args.length > 0) args(0) else "."
     val localRoot: File = new File(path).getCanonicalFile
     localRoot.mkdirs()
     println(s"DEBUG: ${localRoot.getAbsolutePath}")
-
+    val system = ActorSystem()
+    val master = system actorOf Master.props(ConfigFactory.parseString( """"""))
+    //    master ! FirstPairToSync
   }
 }
